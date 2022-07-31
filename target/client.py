22f3fbba93e820dcdf74466c405409114b663aa5
Dimitris Agiakatsikas <agiakatsikas@gmail.com>
2022-07-31 15:20:28 +0300

    sending only dmesg diff

--- target/client.py
+++ target/client.py
@@ -5,82 +5,101 @@ import sys # for exit
 import socket
 import subprocess
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
-
-
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-
-run_dict = {
-    'TIMESTAMP' : '',
-    'STATE' : '',
-    'STDOUT' : '', 
-    'STDERROR': '',
-    'RETURNCODE': '',
-    'DMESG': ''
-}
-
-
-def sys_run(cmd):
-        process = subprocess.run([cmd], capture_output=True, shell=True)
-        return_code = str(process.returncode)
-        stderror = process.stderr.decode("utf-8")
-        stdoutput = process.stdout.decode("utf-8")
-        return return_code, stderror, stdoutput
-
-def time_stamp():
-    now = datetime.now() # current date and time
-    log_date = now.strftime("%m_%d_%Y__%H_%M_%S.%f")[:-3]
-    return log_date
+# configure socket and connect to server  
+clientSocket = socket.socket()  
+#host = socket.gethostname()  
+#port = 25000  
+host = "127.0.0.1"  # Standard loopback interface address (localhost)
+port = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 
 class Fsm(object):
 
-    states = ['state_booted', 'state_warm', 'state_wait', 'state_execute', 'state_error']
-
-    def __init__(self):
-
-
+    states = ['state_wait', 'state_execute', 'state_error']
+    first_run = True
+    dmesg_length = 0
+    def __init__(self, run_command):
+        self.__run_command = run_command
         # Initialize the state machine
-        self.machine = Machine(model=self, states=Fsm.states, initial='state_booted')
+        self.machine = Machine(model=self, states=Fsm.states, initial='state_wait')
+        self.machine.add_transition(trigger='execute', source='state_wait', dest='state_execute', after='execute')
 
-        self.machine.add_transition(trigger='go_warm', source='state_booted', dest='state_warm', after='exec_warm_up')
-        self.machine.add_transition(trigger='go_wait', source=['state_warm', 'state_wait'], dest='state_wait')
-        self.machine.add_transition(trigger='go_execute', source='state_wait', dest='state_execute', after='execute')
+    def sys_run(self, cmd):
+        t1 = datetime.now()
+        process = subprocess.run([cmd], capture_output=True, shell=True)
+        t2 = datetime.now()
+        delta = t1 - t2
+        duration_ms = delta.total_seconds() * 1000
 
-    def exec_warm_up(self):
-        print("Warming")
-        fsm.go_wait()
-        
-  
+        return_code = str(process.returncode)
+        stderror = process.stderr.decode("utf-8")
+        stdoutput = process.stdout.decode("utf-8")
+        return duration_ms, return_code, stderror, stdoutput
+    
+    def get_json(self, state, now, return_code, stderror, stdoutput, dmesg, duration_ms):
+        run_json = {
+            'TIMESTAMP' : '',
+            'STATE' : '',
+            'STDOUT' : '', 
+            'STDERROR': '',
+            'RETURNCODE': '',
+            'DMESG': '',
+            'DURATION_MS': ''
+        }
+        run_json['STATE'] = state
+        run_json['TIMESTAMP'] = now
+        run_json['STDOUT'] = stdoutput
+        run_json['STDERROR'] = stderror
+        run_json['RETURNCODE'] = return_code 
+        run_json['DMESG'] = dmesg
+        run_json['DURATION_MS'] = duration_ms
+        return run_json
+
+
+    def boot_init(self):
+        return_code, stderror, stdoutput = self.sys_run(self.__run_command)
+
+
+    def time_stamp(self):
+        now = datetime.now() # current date and time
+        log_date = now.strftime("%m_%d_%Y__%H_%M_%S.%f")[:-3]
+        return log_date
     def execute(self):
-
         print("Executing benchmark")
-        run_dict['STATE'] = self.state
-        run_dict['TIMESTAMP'] = time_stamp() 
-        cmd = "/usr/bin/sysbench cpu --time=1 --threads=2 run"
-        return_code, stderror, stdoutput = sys_run(cmd)
-        # print("STDOUT: " + stdoutput)
-        # print("STDERROR: " + stderror)
-        # print("RETURNCODE: " + return_code)
-        run_dict['STDOUT'] = stdoutput
-        run_dict['STDERROR'] = stderror
-        run_dict['RETURNCODE'] = return_code
+        
+        dmesg_diff = ""
+        if self.first_run == True:
+            self.first_run = False
+            _, _, _, dmesg = self.sys_run("dmesg")
+            self.dmesg_length = len(dmesg)
+            dmesg_diff = dmesg
+        else:
+            _, _, _, dmesg = self.sys_run("dmesg")
+            dmesg_diff = dmesg[self.dmesg_length: len(dmesg)]
+            self.dmesg_length = len(dmesg)
 
-        return_code, stderror, stdoutput = sys_run("dmesg")
-        dmesg = stdoutput
-        run_dict['DMESG'] = dmesg
+        duration_ms, return_code, stderror, stdoutput = self.sys_run(self.__run_command) 
+        now = self.time_stamp()      
+        print(dmesg_diff)
 
-        s1 = json.dumps(run_dict)
-        fsm.go_wait()
+        
+    
 
 # Global Variables
-fsm = Fsm()
+
+run_command = "/usr/bin/sysbench cpu --time=1 --threads=2 run"
+fsm = Fsm(run_command)
 
 def main():
+
+
+    # clientSocket.connect( ( host, port ) )  
     
-    fsm.go_warm()
+    # # keep track of connection status  
+    # connected = True  
+    # print( "connected to server" )  
 
     while True:
         if fsm.state == 'state_wait':
