e23c81567a7c22fad8e73213d7940c64ee497dd7
Dimitris Agiakatsikas <agiakatsikas@gmail.com>
2022-07-31 22:06:03 +0300

    rpyc working fine. Need to improve dmesg diff

--- host/host.py
+++ host/host.py
@@ -1,79 +1,43 @@
 
 #!/usr/bin/env python
 import sys # for exit
-import socket
-import pickle
+import rpyc
 from time import sleep
 
-# def connect(connected, host, port):
-#     clientSocket = socket.socket()    
-#     while not connected:  
-#         # attempt to reconnect, otherwise sleep for 0.1 seconds  
-#         try:  
-#             clientSocket.connect( ( host, port ) )  
-#             connected = True  
-#             print( "connection successful" )  
-#         except socket.error:
-#             print(socket.error)
-#             connected = False   
-#             sleep( 0.5 ) 
-#         return connected, clientSocket 
-
+def reset():
+    print("ACTION:RESET")
 
 def main():
-
-    host = "127.0.0.1"  # Standard loopback interface address (localhost)
-    port = 65431  # Port to listen on (non-privileged ports are > 1023)
-
-    recon_counter = 0   
-    dic_result = {
-            'TIMESTAMP' : '',
-            'STATE' : '',
-            'STDOUT' : '', 
-            'STDERROR': '',
-            'RETURNCODE': '',
-            'DMESG': '',
-            'DURATION_MS': '',
-            'FIRST_RUN' : ''
-    }
-
-    clientSocket = socket.socket()
-
-    while True:
-        try:  
-            clientSocket.connect( ( host, port ) ) 
-            clientSocket.send(bytes( "execute", "UTF-8" ))
-            payload = b""
-            while True:
-                packet = clientSocket.recv(1024)
-                if not packet: break
-                payload += packet
-            if payload != b"":
-                dic_result = pickle.loads(payload)
-            print(dic_result)
-            clientSocket.close()
-        except socket.error:
-            
-            print( "connection lost... reconnecting" )  
-            while True:  
-                # attempt to reconnect, otherwise sleep for 2 seconds  
+    try_reconn_sec = 5
+    sleep_sec_excep = 0.5 
+    conn_count_thresh =  int(try_reconn_sec / sleep_sec_excep)
+    bench_timeout_sec = 100
+    run_command = "/usr/bin/sysbench cpu --time=1 --threads=2 run"
+    while True:   
+        try:
+            c = rpyc.connect("localhost", 18861)
+            c._config['sync_request_timeout'] = bench_timeout_sec
+            if not c.closed:
                 try:
-                    print( "re-connecting" )   
-                    clientSocket = socket.socket()   
-                    clientSocket.connect( ( host, port ) ) 
-                    print( "re-connection successful" )   
-                    break
-                except socket.error:  
-                    sleep( 1 )  
-        
-                
-
+                    run_dict = c.root.execute(run_command) 
+                    print(run_dict['DURATION_MS'])
+                    c.close()
+                except:
+                    print("ERROR:RUN_TIMEOUT")
+                    reset()              
+        except:
+            conn_count_thresh =  conn_count_thresh - 1
+            print("connection_error_counter_thresh: " + str(conn_count_thresh))
+            sleep(0.5)
+            if conn_count_thresh <=0:
+                reset()
+                conn_count_thresh =  int(try_reconn_sec / sleep_sec_excep)
+                          
 if __name__ == '__main__':
     try:
         print("Press Ctrl-C to terminate")   
         proc = main()
     except KeyboardInterrupt:
         print('Exiting program')
-        socket.close()
         sys.exit
         pass
--- target/dut.py
+++ target/dut.py
@@ -1,39 +1,35 @@
-from multiprocessing.reduction import ACKNOWLEDGE
-from transitions import Machine
+import rpyc
+
 from time import sleep
 import sys # for exit
-import socket
 import subprocess
-import pickle
-from datetime import datetime, timedelta 
+from datetime import datetime, timedelta
 
-class Fsm(object):
 
-    def __init__(self, run_command):
-        self.states = ['state_wait', 'state_execute', 'state_error']
+
+class ExecuteService(rpyc.Service):
+
+    def __init__(self):
         self.first_run = True
         self.dmesg_length = 0
-        self.__run_command = run_command
-        # Initialize the state machine
-        self.machine = Machine(model=self, states=self.states, initial='state_wait')
-        self.machine.add_transition(trigger='execute', source='state_wait', dest='state_execute', before='receive_cmd', after='execute')
+        self.iteration = 0
 
-    def sys_run(self, cmd):
-        t1 = datetime.now()
-        process = subprocess.run([cmd], capture_output=True, shell=True)
-        t2 = datetime.now()
-        delta = t2 - t1
-        duration_ms = round(delta.total_seconds() * 1000)
+    def on_connect(self, conn):
+        # code that runs when a connection is created
+        # (to init the service, if needed)
+        print("Client Connected")
+        pass
 
-        return_code = str(process.returncode)
-        stderror = process.stderr.decode("utf-8")
-        stdoutput = process.stdout.decode("utf-8")
-        return duration_ms, return_code, stderror, stdoutput
-    
-    def get_json(self, state, now, return_code, stderror, stdoutput, dmesg, duration_ms, first_run):
-        run_json = {
+    def on_disconnect(self, conn):
+        # code that runs after the connection has already closed
+        # (to finalize the service, if needed)
+        print("Client Disconnected")
+        pass
+
+    def get_dict(self, run_command, now, return_code, stderror, stdoutput, dmesg, duration_ms, first_run):
+        run_dict = {
+            'RUN_COMMAND' : '',
             'TIMESTAMP' : '',
-            'STATE' : '',
             'STDOUT' : '', 
             'STDERROR': '',
             'RETURNCODE': '',
@@ -41,28 +37,30 @@ class Fsm(object):
             'DURATION_MS': '',
             'FIRST_RUN' : ''
         }
-        run_json['STATE'] = state
-        run_json['TIMESTAMP'] = now
-        run_json['STDOUT'] = stdoutput
-        run_json['STDERROR'] = stderror
-        run_json['RETURNCODE'] = return_code 
-        run_json['DMESG'] = dmesg
-        run_json['DURATION_MS'] = duration_ms
-        run_json['FIRST_RUN'] = first_run
-        return run_json
+        run_dict['RUN_COMMAND'] = now
+        run_dict['TIMESTAMP'] = now
+        run_dict['STDOUT'] = stdoutput
+        run_dict['STDERROR'] = stderror
+        run_dict['RETURNCODE'] = return_code 
+        run_dict['DMESG'] = dmesg
+        run_dict['DURATION_MS'] = duration_ms
+        run_dict['FIRST_RUN'] = first_run
+        return run_dict
 
 
-    def boot_init(self):
-        return_code, stderror, stdoutput = self.sys_run(self.__run_command)
-
-
+    # My service
+    def execute(self):
+        print("Execute Request")
+        return self.execute()
+    
     def time_stamp(self):
         now = datetime.now() # current date and time
-        log_date = now.strftime("%m_%d_%Y__%H_%M_%S.%f")[:-3]
+        log_date = now.strftime("%m_%d_%Y__%H_%M_%S.%f")[:-3] 
         return log_date
 
-    def execute(self):
-        print("Executing benchmark")
+    def exposed_execute(self, run_command):
+        print("Run[" + str(self.iteration) + "]: " + run_command)
+        self.iteration = self.iteration + 1
         dmesg_diff = ""
         if self.first_run == True:
             self.first_run = False
@@ -73,51 +71,39 @@ class Fsm(object):
             _, _, _, dmesg = self.sys_run("dmesg")
             dmesg_diff = dmesg[self.dmesg_length: len(dmesg)]
             self.dmesg_length = len(dmesg)
-        duration_ms, return_code, stderror, stdoutput = self.sys_run(self.__run_command) 
-        now = self.time_stamp()    
-        first_run = "1"
-        dic_result = self.get_json(self.state, now, return_code, stderror, stdoutput, dmesg_diff, duration_ms, first_run)
+        duration_ms, return_code, stderror, stdoutput = self.sys_run(run_command) 
+        now = self.time_stamp()
+        first_run = "0" 
+        if self.first_run == True:
+            first_run = "1"
+
+        dic_result = self.get_dict(run_command, now, return_code, stderror, stdoutput, dmesg_diff, duration_ms, first_run)
         return dic_result
-
-        
-
-# Global Variables
-
-
-
-def main():
-    run_command = "/usr/bin/sysbench cpu --time=1 --threads=2 run"
-    fsm = Fsm(run_command)
-
-    host = "127.0.0.1"  # Standard loopback interface address (localhost)
-    port = 65431  # Port to listen on (non-privileged ports are > 1023)
-
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind((host, port))
-    serverSocket.listen()
-    connection, _ = serverSocket.accept()
-
-    while True:  
-        # attempt to send and receive wave, otherwise reconnect  
-        try:
-            message = connection.recv( 1024 ).decode( "UTF-8" )
-            if message == "execute":
-                dic_result = fsm.execute() 
-                payload = pickle.dumps(dic_result)
-                connection.sendall(payload)
-        except socket.error:  
-            serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            serverSocket.bind((host, port))
-            serverSocket.listen()
-            connection, _ = serverSocket.accept()
-
+    
+    def sys_run(self, cmd):
+        t1 = datetime.now()
+        process = subprocess.run([cmd], capture_output=True, shell=True)
+        t2 = datetime.now()
+        delta = t2 - t1
+        duration_ms = round(delta.total_seconds() * 1000)
+        return_code = str(process.returncode)
+        stderror = process.stderr.decode("utf-8")
+        stdoutput = process.stdout.decode("utf-8")
+        return duration_ms, return_code, stderror, stdoutput
 
 if __name__ == '__main__':
     try:
-        print("Press Ctrl-C to terminate")   
-        proc = main()
+        print("Press Ctrl-C to terminate")
+        from rpyc.utils.server import ThreadPoolServer 
+        server = ThreadPoolServer(ExecuteService, port=18861)
+        server.start()
+        #server = ThreadPoolServer(ExecuteService, port=18861, nbThreads=1, requestBatchSize=1)
+        #server.start()
+        # while True:
+        #     server = OneShotServer(ExecuteService, port=18861)
+        #     server.start()
     except KeyboardInterrupt:
         print('Exiting program')                  
-        clientSocket.close();  
+        server.close()
         sys.exit
         pass
