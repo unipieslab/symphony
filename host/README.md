80ec12574ca3927ce640938a0c805f1b2bc34929
Dimitris Agiakatsikas <agiakatsikas@gmail.com>
2022-08-04 00:34:10 +0300

    host return call back not working

---
+++ host/README.md
@@ -0,0 +1,6 @@
+#Problems
+Monitor power and temperature while running a benhmark is not working
+
+#Questions
+Q1) Do we need to monitor the power and temp during the execution of a benchmark or simply to report voltage and freq. at the end of the execution?
+Q2) Could you provide a non safe voltage to test the framework ?
--- host/host.py
+++ host/host.py
@@ -5,6 +5,14 @@ import rpyc
 from time import sleep
 import serial
 from serial.tools import list_ports #pyserial, esptool
+import json
+
+#Global variables
+command_timeout_counter = 0
+connection_timeout_counter = 0
+sdc_counter = 0
+reset_counter = 0
+power_cycle_counter = 0
 
 def find_reset_uart():
     VID = '0403'
@@ -42,43 +50,94 @@ def find_reset_uart():
 ser = find_reset_uart()
 
 def power_button():
-    ser.dtr = True
-    sleep(2)
-    ser.dtr = False
-    print("ACTION:POWER")
+    # ser.dtr = True
+    # sleep(2)
+    # ser.dtr = False
+    # ser.dtr = True
+    # sleep(2)
+    # ser.dtr = False
+    print("ACTION:POWER_CYCLE")
 
 
 def reset_button():
-    ser.rts = True
-    sleep(1)
-    ser.rts = False
+    # ser.rts = True
+    # sleep(1)
+    # ser.rts = False
+
     print("ACTION:RESET")
 
-def main():
+
+def remote_execute(command:str, command_timeout_sec:int, dmesg_index:int):
     try_reconn_sec = 5
     sleep_sec_excep = 0.5 
     conn_count_thresh =  int(try_reconn_sec / sleep_sec_excep)
-    bench_timeout_sec = 2
-    run_command = "/usr/bin/sysbench cpu --time=1 --threads=2 run"
-    while True:   
-        try:
-            c = rpyc.connect("localhost", 18861)
-            c._config['sync_request_timeout'] = bench_timeout_sec
-            if not c.closed:
-                try:
-                    run_dict = c.root.execute(run_command) 
-                    print(run_dict['DURATION_MS'])
-                    c.close()
-                except:
-                    print("ERROR:RUN_TIMEOUT")
-                    reset_button()              
-        except:
-            conn_count_thresh =  conn_count_thresh - 1
-            print("connection_error_counter_thresh: " + str(conn_count_thresh))
-            sleep(0.5)
-            if conn_count_thresh <=0:
-                reset_button()
-                conn_count_thresh =  int(try_reconn_sec / sleep_sec_excep)
+    target_ip = "10.30.0.100" #"localhost"
+    try:
+        c = rpyc.connect(target_ip, 18861)
+        c._config['sync_request_timeout'] = command_timeout_sec
+        if not c.closed:
+            try:
+                run_dict = c.root.execute(command, dmesg_index)
+                print(run_dict['STDOUT'] )
+                c.close()
+                return run_dict
+            except:
+                print("ERROR:RUN_TIMEOUT")
+                command_timeout_counter += 1
+                reset_button()              
+    except:
+        conn_count_thresh =  conn_count_thresh - 1
+        print("connection_error_counter_thresh: " + str(conn_count_thresh))
+        sleep(0.5)
+        if conn_count_thresh <=0:
+            command_timeout_counter += 1
+            reset_button()
+            conn_count_thresh =  int(try_reconn_sec / sleep_sec_excep)
+    return get_empty_dict()
+
+def get_empty_dict():
+        run_dict = {
+            'RUN_COMMAND' : '',
+            'TIMESTAMP' : '',
+            'STDOUT' : '', 
+            'STDERROR': '',
+            'RETURNCODE': '',
+            'DMESG': '',
+            'DURATION_MS': ''
+        }
+        return run_dict
+
+def get_golden():
+    benchmarks_list = ["MG", "CG", "CG", "IS", "LU", "EP"]
+
+    benchmark_commands = {
+        "MG" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/mg.A.8',
+        "CG" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/cg.A.8',
+        "CG" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/ft.A.8', 
+        "IS" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/is.A.8',
+        "LU" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/lu.A.8',
+        "EP" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/ep.A.8'
+}
+
+    for id in benchmarks_list:
+            command = benchmark_commands[id]
+            run_dict = remote_execute(command, 240, 1)
+            golden_info = run_dict['STDOUT']
+            execution_time_info = run_dict['DURATION_MS']
+            info = {
+                "command": command,
+                "golden" : golden_info,
+                "execution_time" : execution_time_info
+            }
+            json_path = "golden/" + id + ".json"
+            with open(json_path, "w") as outfile:
+                json.dump(info, outfile)
+
+def main():
+    get_golden()
+    # while True:   
+    #     run_dict = remote_execute(command, 240)
+    #     print 
                           
 if __name__ == '__main__':
     try:
