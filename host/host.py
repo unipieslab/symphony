c4ab87493f2f89dfe281f7e003de69ebda041f91
Dimitris Agiakatsikas <agiakatsikas@gmail.com>
2022-08-04 14:48:05 +0300

    current state

--- host/host.py
+++ host/host.py
@@ -14,10 +14,17 @@ sdc_counter = 0
 reset_counter = 0
 power_cycle_counter = 0
 
-def find_reset_uart():
-    VID = '0403'
-    PID = '6001'
-    SERIAL_NUM = 'A50285BI'
+def find_reset_uart(VID:str, PID:str, SERIAL_NUM:str):
+    """This function finds the specific UART that is used for resetting and power cycling the XGENE-2
+
+    Args:
+        VID (str): USB2UART Vendor ID
+        PID (str): USB2UART Product ID
+        SERIAL_NUM (str): Self explained
+
+    Returns:
+        serial.Serial(): Returns the uart driver
+    """    
     port = None
     device_list = list_ports.comports()
     for device in device_list:
@@ -47,7 +54,10 @@ def find_reset_uart():
             while True:
                 pass
     
-ser = find_reset_uart()
+VID = '0403'
+PID = '6001'
+SERIAL_NUM = 'A50285BI'
+#ser = find_reset_uart(VID, PID, SERIAL_NUM)
 
 def power_button():
     # ser.dtr = True
@@ -66,50 +76,46 @@ def reset_button():
 
     print("ACTION:RESET")
 
+def get_empty_dict():
+    run_dict = {
+        'RUN_COMMAND' : '',
+        'TIMESTAMP' : '',
+        'STDOUT' : '', 
+        'STDERROR': '',
+        'RETURNCODE': '',
+        'DMESG': '',
+        'DURATION_MS': ''
+    }
+    return run_dict
 
-def remote_execute(command:str, command_timeout_sec:int, dmesg_index:int):
+def remote_execute(command:str, command_timeout_sec:int):
     try_reconn_sec = 5
     sleep_sec_excep = 0.5 
     conn_count_thresh =  int(try_reconn_sec / sleep_sec_excep)
     target_ip = "10.30.0.100" #"localhost"
+    run_dict = get_empty_dict()
     try:
         c = rpyc.connect(target_ip, 18861)
         c._config['sync_request_timeout'] = command_timeout_sec
         if not c.closed:
             try:
-                run_dict = c.root.execute(command, dmesg_index)
-                print(run_dict['STDOUT'] )
-                c.close()
-                return run_dict
+                run_dict = c.root.execute(command)      
             except:
                 print("ERROR:RUN_TIMEOUT")
-                command_timeout_counter += 1
-                reset_button()              
+                c.close() 
+                reset_button()
+        c.close()    
     except:
         conn_count_thresh =  conn_count_thresh - 1
         print("connection_error_counter_thresh: " + str(conn_count_thresh))
         sleep(0.5)
         if conn_count_thresh <=0:
-            command_timeout_counter += 1
             reset_button()
             conn_count_thresh =  int(try_reconn_sec / sleep_sec_excep)
-    return get_empty_dict()
+    return run_dict
 
-def get_empty_dict():
-        run_dict = {
-            'RUN_COMMAND' : '',
-            'TIMESTAMP' : '',
-            'STDOUT' : '', 
-            'STDERROR': '',
-            'RETURNCODE': '',
-            'DMESG': '',
-            'DURATION_MS': ''
-        }
-        return run_dict
-
-def get_golden():
+def generate_golden():
     benchmarks_list = ["MG", "CG", "CG", "IS", "LU", "EP"]
-
     benchmark_commands = {
         "MG" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/mg.A.8',
         "CG" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/cg.A.8',
@@ -117,27 +123,37 @@ def get_golden():
         "IS" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/is.A.8',
         "LU" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/lu.A.8',
         "EP" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/ep.A.8'
-}
+    }
 
-    for id in benchmarks_list:
-            command = benchmark_commands[id]
-            run_dict = remote_execute(command, 240, 1)
-            golden_info = run_dict['STDOUT']
-            execution_time_info = run_dict['DURATION_MS']
-            info = {
-                "command": command,
-                "golden" : golden_info,
-                "execution_time" : execution_time_info
-            }
-            json_path = "golden/" + id + ".json"
-            with open(json_path, "w") as outfile:
-                json.dump(info, outfile)
+    # for id in benchmarks_list:
+    #         command = benchmark_commands[id]
+    #         run_dict = remote_execute(command, 100)
+    #         golden_info = run_dict['STDOUT']
+    #         execution_time_info = run_dict['DURATION_MS']
+    #         info = {
+    #             "command": command,
+    #             "golden" : golden_info,
+    #             "execution_time" : execution_time_info
+    #         }
+    #         json_path = "golden/" + id + ".json"
+    #         with open(json_path, "w") as outfile:
+    #             json.dump(info, outfile)
 
 def main():
-    get_golden()
-    # while True:   
-    #     run_dict = remote_execute(command, 240)
-    #     print 
+    # generate_golden()
+    benchmarks_list = ["MG", "CG", "CG", "IS", "LU", "EP"]
+    benchmark_commands = {
+        "MG" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/mg.A.8',
+        "CG" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/cg.A.8',
+        "CG" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/ft.A.8', 
+        "IS" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/is.A.8',
+        "LU" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/lu.A.8',
+        "EP" : 'mpirun --allow-run-as-root --cpu-set 0-7 -np 8 --mca btl ^openib /opt/bench/NPB/NPB3.3-MPI/bin/ep.A.8'
+    }   
+    while True:
+        command = benchmark_commands["MG"]
+        run_dict = remote_execute(command, 10)
+        print(run_dict["DURATION_MS"])
                           
 if __name__ == '__main__':
     try:
