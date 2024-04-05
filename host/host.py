bd2ce905c46d345270cffc2fe5ac0eea1725d8ea
constarg <argyrioukost@gmail.com>
2024-04-05 18:50:25 +0300

    Fix a few issues

--- host/host.py
+++ host/host.py
@@ -199,11 +199,11 @@ class Tester_Shell:
         self.__benchmark_timeout = 2 * round(self.__batch_per_benchmark[self.__current_benchmark_id] * self.__timeouts[self.__current_benchmark_id])
         self.__benchmark_cold_cache_timeout = round(self.__timeouts[self.__current_benchmark_id] * Tester_Shell_Constants.TIMEOUT_COLD_CACHE_SCALE_BENCHMARK.value)
 
-        #self.__target_set_voltage()
-        #self.__target_set_frequency()
+        self.__target_set_voltage()
+        self.__target_set_frequency()
 
     def __target_set_voltage(self):
-        self.logging.info('Configuring voltage: ' + self.__current_voltage_id)
+        logging.warning('Configuring voltage: ' + self.__current_voltage_id)
         self.__remote_execute(self.__voltage_commands[self.__current_voltage_id], self.__voltage_config_timeout,
                               Tester_Shell_Constants.NETWORK_TIMEOUT_SEC.value, 1, 1, False)[0]
 
@@ -229,6 +229,7 @@ class Tester_Shell:
                 c._config['sync_request_timeout'] = excp_timeout_s
 
                 if not c.closed:
+                    logging.info("Connected to server")
                     return c
             
                 return None
@@ -238,7 +239,7 @@ class Tester_Shell:
                 first_error = False
                 conn_count_thresh -= 1 
                 attemp_counter += 1
-                self.logging.warning('Remote is down..trying to connect. Attempt: ' + str(attemp_counter))
+                logging.warning('Remote is down..trying to connect. Attempt: ' + str(attemp_counter))
                 sleep(sleep_sec_excep)
                 if conn_count_thresh <= 0:
                     if (ret_imediate == True):
@@ -299,19 +300,13 @@ class Tester_Shell:
         """
         conn = self.__target_connect_common(boot_timeout_s, net_timeout_s, ret_imediate)
         alive: bool = False
-        start = timeit.default_timer()
-
-        self.logging.info('Checking if remote is up')
-
+        logging.info('Checking if remote is up')
         if conn == None:
             return False
         
         try:
-            alive = conn.alive()
-            
-            self.logging.info('Remote is up')
-            time = str(self.math.ceil(timeit.default_timer() - start))
-            self.logging.info("Boot time elapsed (seconds): " + time) 
+            alive = conn.root.alive()
+            logging.info('Remote is up')
 
             conn.close()
             return alive
@@ -319,7 +314,7 @@ class Tester_Shell:
             conn.close()
 
     def __remote_execute(self, cmd, cmd_timeout_s: int, net_timeout_s: int, dmesg_index: int, times: int, ret_imediate: bool):
-        alive = self.__remote_alive(self.__boot_timeout_sec, net_timeout_s)
+        alive = self.__remote_alive(self.__boot_timeout_sec, net_timeout_s, ret_imediate)
         if (not alive and ret_imediate == True):
             return
         elif (not alive):
@@ -345,15 +340,15 @@ class Tester_Shell:
                     new_dicts = dict(results_list)
                     results.append(new_dicts)
 
-                time = str(self.math.ceil(timeit.default_timer() - start))
-                self.logging.info("Remote_execute(" + results[0]["run_command"] + ") elapsed (seconds): " + time)
+                time = str(math.ceil(timeit.default_timer() - start))
+                logging.info("Remote_execute(" + results[0]["run_command"] + ") elapsed (seconds): " + time)
                 for check_result in results:
                     if check_result["return_code"] != '0':
-                        self.logging.error("ERROR WHEN RUNNING: " + check_result["run_command"] + " STDERR: " + check_result["stderror"])
+                        logging.error("ERROR WHEN RUNNING: " + check_result["run_command"] + " STDERR: " + check_result["stderror"])
                 conn.close()
                 return results
 
-            except:
+            except Exception as e:
                 if first_error == True:
                     self._ovrd_clacify_detected_error()
                 first_error = False
@@ -362,7 +357,7 @@ class Tester_Shell:
                     self._ovrd_unused_target_reset_button()
                     first_error = True
                 else:
-                    self.logging.warning("Execution timeout. Attempt " + str(execution_attempt_counter))
+                    logging.warning("Execution timeout. Attempt " + str(execution_attempt_counter))
                 execution_attempt_counter += 1
 
     def __load_optional_attr_from_dict(self, src: dict):
@@ -376,17 +371,62 @@ class Tester_Shell:
             finish_after_total_errors: float        = src["finish_after_total_errors"]
         except KeyError as e:
             self.__effective_time_per_batch_s       = Tester_Shell_Constants.EFFECTIVE_SEC_PER_BATCH.value
-            self.__finish_after_total_effective_min = Tester_Shell_Defaults.FINISH_AFTER_TOTAL_EFFECTIVE_MINUTES
-            self.__finish_after_total_errors        = Tester_Shell_Defaults.FINISH_AFTER_TOTAL_ERRORS
+            self.__finish_after_total_effective_min = Tester_Shell_Defaults.FINISH_AFTER_TOTAL_EFFECTIVE_MINUTES.value
+            self.__finish_after_total_errors        = Tester_Shell_Defaults.FINISH_AFTER_TOTAL_ERRORS.value
             logging.warning("Unable to load optional attributes. Using defaults instead.")
-            logging.warning("Missing value for: " + str(e.args[0]))
+            logging.warning("Missing value for: " + str(e.args[0]) + " -> (Dictionary/JSON)")
 
         if (effective_time_per_batch_s != None):
+            logging.warning("Setting EFFECTIVE_TIME_PER_BATCH = " + str(effective_time_per_batch_s))
             self.__effective_time_per_batch_s = effective_time_per_batch_s
+
         if (finish_after_total_effective_min != None):
+            logging.warning("Setting FINISH_AFTER_TOTAL_EFFECTIVE_MIN = " + str(finish_after_total_effective_min))
             self.__finish_after_total_effective_min = finish_after_total_effective_min
+
         if (finish_after_total_errors != None):
+            logging.warning("Setting FINISH_AFTER_TOTAL_ERRORS = " + str(finish_after_total_errors))
             self.__finish_after_total_errors = finish_after_total_errors
+
+    def __validate_attr_on_dict(self, src: dict):
+        try:
+            [self.__voltage_commands[vid] for vid in self.__voltage_list]
+            [self.__benchmark_commands[bid] for bid in self.__benchmark_list]
+            [self.__timeouts[vid] for vid in self.__voltage_list]
+            [self.__timeouts[bid] for bid in self.__benchmark_list]
+        except KeyError as e:
+            logging.error("The data on Dictionary/JSON is incomplete")
+            logging.error("Incomplete value: " + str(e.args[0]) + " -> Dictionary/JSON")
+            logging.error("Ensure the incomplete value is present in the relevant lists and dictionaries.")
+            exit(0)        
+
+    def __target_set_next_voltage(self):
+        """
+        """
+        curr_vid_index = self.__voltage_list.index(self.__current_voltage_id)
+        next_vid_index = curr_vid_index + 1
+        if next_vid_index >= len(self.__voltage_list):
+            return False
+        
+        logging.warning("Setting CURRENT_VOLTAGE_ID = " + self.__voltage_list[next_vid_index])
+        self.__current_voltage_id = self.__voltage_list[next_vid_index]
+        self.__target_set_voltage()
+
+        return True
+
+    def __target_set_next_benchmark(self):
+        """
+        """
+        curr_bid_index = self.__benchmark_list.index(self.__current_benchmark_id)
+        next_bid_index = curr_bid_index + 1
+        if next_bid_index >= len(self.__benchmark_list):
+            return False
+
+        logging.warning("Setting CURRENT_BENCHMARK_ID = " + self.__benchmark_list[next_bid_index])
+        self.__current_benchmark_id = self.__benchmark_list[next_bid_index]
+        self.__update()
+
+        return True
 
     """
         <--- Methods for every implementation --->
@@ -406,8 +446,8 @@ class Tester_Shell:
             self.__target_port        = src["target_port"]
             self.__setup_id           = src["setup_id"]
         except KeyError as e:
-            logging.error("Failed to load attributes from dictionary or json")
-            logging.error("Missing value for: " + str(e.args[0]))
+            logging.error("Failed to parse dictionary/JSON")
+            logging.error("Missing value for: " + str(e.args[0]) + " -> Dictionary/JSON")
             exit(0)
         
         self.__load_optional_attr_from_dict(src)
@@ -416,6 +456,9 @@ class Tester_Shell:
         self.__current_benchmark_id = self.__benchmark_list[0]
         self.__current_voltage_id   = self.__voltage_list[0]
 
+        logging.warning("Setting CURRENT_BENCHMARK_ID = " + self.__current_benchmark_id)
+        logging.warning("Setting CURRENT_VOLTAGE_ID = " + self.__current_voltage_id)
+
         # Calculate the number of runs per batch for each benchmark.
         # And initialize the system/network errors per benchmark
         for benchmark in self.__benchmark_list:
@@ -423,8 +466,8 @@ class Tester_Shell:
             self.__system_errors_per_benchmark[benchmark]  = 0
             self.__network_errors_per_benchmark[benchmark] = 0
 
+        self.__validate_attr_on_dict(src)
         logging.info("Attributes parsed successfully from dictionary/JSON")
-
         # Update the attributes of the Tester.
         self.__update()
 
@@ -433,32 +476,21 @@ class Tester_Shell:
             json_content: dict = json.load(json_file)
             self.load_experiment_attr_from_dict(json_content)
 
-    def target_set_next_voltage(self):
-        """
-        """
-        curr_vid_index = self.__voltage_list.index(self.__current_voltage_id)
-        next_vid_index = curr_vid_index + 1
-        if next_vid_index > len(self.__voltage_list):
-            return False
+    def target_perform_undervolt_test(self):
+        logging.warning("Start the undervolting process for: " + self.__target_ip)
 
-        self.__current_voltage_id = self.__voltage_list[next_vid_index]
-        self.__target_set_voltage()
+        benchmarks_has_next = True
+        voltage_has_next    = True
+        while voltage_has_next:
+            # TODO - start experiment for the current benchmark.
+            benchmarks_has_next = self.__target_set_next_benchmark()
+            if not benchmarks_has_next:
+                # Reset benchmarks
+                logging.warning("Setting CURRENT_BENCHMARK_ID = " + self.__benchmark_list[0])
 
-        return True
+                self.__current_benchmark_id = self.__benchmark_list[0]
+                voltage_has_next = self.__target_set_next_voltage()
 
-    def target_set_next_benchmark(self):
-        """
-        """
-        curr_bid_index = self.__benchmark_list.index(self.__current_benchmark_id)
-        next_bid_index = curr_bid_index + 1
-        if next_bid_index > len(self.__benchmark_list):
-            return False
-
-        self.__current_benchmark_id = self.__benchmark_list[next_bid_index]
-        self.__update()
-
-        return True
-    
     def target_set_next_frequency(self):
         pass
 
@@ -481,11 +513,12 @@ class Tester_Shell:
         if action == Tester_Shell_Power_Action.TARGET_POWER_BTN_PRESS:
             self._ovrd_unused_target_power_button()
         elif action == Tester_Shell_Power_Action.TARGET_RESET_BTN_PRESS:
-            self._ovrd_unused_target_reset_button()
-            alive = self.__remote_alive(self.__boot_timeout_sec, Tester_Shell_Constants.NETWORK_TIMEOUT_SEC.value)
+            alive = False
             while not alive:
-                # TODO - is this right? rethink it. What if the computer never turn on again.
-                alive = self.__remote_alive(self.__boot_timeout_sec, Tester_Shell_Constants.NETWORK_TIMEOUT_SEC.value)
+                self._ovrd_unused_target_reset_button()
+                sleep(self.__boot_timeout_sec)
+                alive = self.__remote_alive(self.__boot_timeout_sec, Tester_Shell_Constants.NETWORK_TIMEOUT_SEC.value, True)
+            self.logging.info("Booted")
 
         self.__target_set_voltage()
         self.__target_set_frequency()
@@ -585,6 +618,8 @@ class Tester_Shell:
 def main():
     test = Tester_Shell()
     test.load_experiment_attr_from_json_file("test.json")
+
+    test.target_perform_undervolt_test()
 
 if __name__ == '__main__':
     main()
